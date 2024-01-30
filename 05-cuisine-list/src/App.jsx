b270d42ea3434204905a08fe46991688759a9d2d
ishika2203 <ishikabatra.net@gmail.com>
2024-01-30 16:47:33 +0530

    categories added

--- 05-cuisine-list/src/App.jsx
+++ 05-cuisine-list/src/App.jsx
@@ -3,8 +3,17 @@ import data from './data';
 import Menu from './Menu';
 import Title from './Title';
 
+// const tempCategories = data.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ['all', ...tempSet];
+// console.log(tempItems);
+
+const allCategories = ['all', ...new Set(data.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(data);
+  const [categories, setCategories] = useState(allCategories);
+  console.log(categories);
   return (
     <main>
       <section className="menu">
