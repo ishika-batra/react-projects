015b87d2b7c34e403becd4e9e36f163e553f5e02
ishika2203 <ishikabatra.net@gmail.com>
2024-01-31 01:55:28 +0530

    duties added

--- 06-sections/src/App.jsx
+++ 06-sections/src/App.jsx
@@ -1,25 +1,29 @@
 import { useEffect } from 'react';
 import { useState } from 'react';
 import JobInfo from './JobInfo';
+import data from './data';
 
 const url = 'https://course-api.com/react-tabs-project';
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [jobs, setJobs] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  // uncomment
+  const [jobs, setJobs] = useState(data);
   // current item
 
   const fetchJobs = async () => {
     const response = await fetch(url);
     const newJobs = await response.json();
     setJobs(newJobs);
+
     setIsLoading(false);
   };
 
-  useEffect(() => {
-    fetchJobs();
-  });
+  // useEffect(() => {
+  //   fetchJobs();
+  // }, []);
 
+  console.log(isLoading);
   if (isLoading) {
     return (
       <section className="jobs-center">
