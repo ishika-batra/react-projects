3e85e8dbea4d8552884d55a2ca4e84973346ccb7
ishika2203 <ishikabatra.net@gmail.com>
2024-01-30 18:25:51 +0530

    filter item functionality added

--- 05-cuisine-list/src/App.jsx
+++ 05-cuisine-list/src/App.jsx
@@ -14,12 +14,20 @@ const allCategories = ['all', ...new Set(data.map((item) => item.category))];
 const App = () => {
   const [menuItems, setMenuItems] = useState(data);
   const [categories, setCategories] = useState(allCategories);
+  const filterItems = (category) => {
+    if (category === 'all') {
+      setMenuItems(data);
+      return;
+    }
+    const newItems = data.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
 
   return (
     <main>
       <section className="menu">
         <Title text="Our Menu" />
-        <Categories categories={categories} />
+        <Categories categories={categories} filterItems={filterItems} />
         <Menu items={menuItems} />
       </section>
     </main>
