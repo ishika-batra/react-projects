098416c86cd0bc2aa98f702dfe3a8eaa9fac249f
ishika2203 <ishikabatra.net@gmail.com>
2024-02-06 23:56:24 +0530

    structure of random text generator project added

---
+++ 08-random-text-generator/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react';
+import data from './data';
+
+const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn" type="submit">
+          Generate
+        </button>
+      </form>
+    </section>
+  );
+};
+export default App;
