8f5c528adee14649ef1b5f10d43e0f4d2a92454d
ishika2203 <ishikabatra.net@gmail.com>
2024-02-08 23:59:43 +0530

    values.js library added

--- 09-palette-play/src/App.jsx
+++ 09-palette-play/src/App.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import ColorList from './ColorList';
 import Form from './Form';
+import Values from 'values.js';
 
+const [colors, setColors] = useState([]);
+console.log(new Values('#f15025').all(10));
 const App = () => {
   return (
     <main>
