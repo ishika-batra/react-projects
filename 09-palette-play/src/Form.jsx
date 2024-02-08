d100c1438a181029b72a2396cbceb8e45211c407
ishika2203 <ishikabatra.net@gmail.com>
2024-02-08 23:51:06 +0530

    form structure added

--- 09-palette-play/src/Form.jsx
+++ 09-palette-play/src/Form.jsx
@@ -1,4 +1,24 @@
+import { useState } from 'react';
+
 const Form = () => {
-  return <div>Form</div>;
+  const [color, setColor] = useState('');
+  const handleSubmit = (e) => {};
+
+  return (
+    <section className="container">
+      <h4>Palette Play</h4>
+      <form className="color-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={color}
+          onChange={(e) => setColor(e.target.value)}
+          placeholder="#f15025"
+        />
+        <button className="btn" type="submit" style={{ background: color }}>
+          Submit
+        </button>
+      </form>
+    </section>
+  );
 };
 export default Form;
